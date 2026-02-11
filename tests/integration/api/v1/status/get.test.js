eec189b80fb6406d6ad341b3f94e5bdeb6b1001c
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-02-11 08:04:36 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,7 +10,9 @@ async function cleanDatabase() {
   await database.query(`drop schema public cascade; create schema public;`);
 }
 
-test('GET /api/v1/status should return 200', async () => {
+describe('GET /api/v1/status', () => {
+describe('Anonymous user', () => {
+test('Retrieving current system status', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
   const data = await response.json();
@@ -28,4 +30,6 @@ test('GET /api/v1/status should return 200', async () => {
   expect(data.dependencies.database.used_connections).toEqual(1);
   const parsedUpdatedAt = new Date(data.updated_at).toISOString();
   expect(data.updated_at).toEqual(parsedUpdatedAt);
+})
+})
 });
