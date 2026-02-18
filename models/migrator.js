91d0346a2a8c10db541f3057167986b6fd359dc4
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-02-18 14:03:02 -0300

    style: fix lint:prettier

--- models/migrator.js
+++ models/migrator.js
@@ -4,57 +4,55 @@ import { resolve } from 'path';
 import { ServiceError } from 'infra/errors';
 
 const defaultMigrationOptions = {
-    dir: resolve('infra', 'migrations'),
-    dryRun: true,
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  };
+  dir: resolve('infra', 'migrations'),
+  dryRun: true,
+  direction: 'up',
+  verbose: true,
+  migrationsTable: 'pgmigrations',
+};
 
-  async function listPendingMigrations() {
-    let dbClient;
-    try {
-      dbClient = await database.getNewClient();
-      const pendingMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dbClient,
-      });
-      return pendingMigrations
-    } catch(error) {
-      throw new ServiceError({
-          message: "Erro ao listar migrações pendentes.",
-          cause: error,
-        });
-    } 
-    finally {
-      await dbClient?.end();
-    }
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return pendingMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: 'Erro ao listar migrações pendentes.',
+      cause: error,
+    });
+  } finally {
+    await dbClient?.end();
   }
+}
 
-  async function runPendingMigrations() {
-    let dbClient;
-    try {
-      dbClient = await database.getNewClient();
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dbClient,
-        dryRun: false,
-      });
-      return migratedMigrations
-    } catch(error) {
-        throw new ServiceError({
-            message: "Erro ao executar migrações pendentes.",
-            cause: error,
-          });
-      } 
-      finally {
-        await dbClient?.end();
-      }
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+    return migratedMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: 'Erro ao executar migrações pendentes.',
+      cause: error,
+    });
+  } finally {
+    await dbClient?.end();
   }
+}
 
-  const migrator = {
-    listPendingMigrations,
-    runPendingMigrations
-  }
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
 
-  export default migrator
+export default migrator;
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -10,16 +10,14 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
-    const pendingMigrations = await migrator.listPendingMigrations();
-    res.status(200).json(pendingMigrations);
-  
+  const pendingMigrations = await migrator.listPendingMigrations();
+  res.status(200).json(pendingMigrations);
 }
 
 async function postHandler(req, res) {
-    const migratedMigrations = await migrator.runPendingMigrations();
-    if (migratedMigrations.length > 0) {
-      res.status(201).json(migratedMigrations);
-    }
-    res.status(200).json(migratedMigrations);
-
+  const migratedMigrations = await migrator.runPendingMigrations();
+  if (migratedMigrations.length > 0) {
+    res.status(201).json(migratedMigrations);
+  }
+  res.status(200).json(migratedMigrations);
 }
