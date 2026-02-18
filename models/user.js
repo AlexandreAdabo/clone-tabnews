f00e4d0ae5142e908e10ded1cc36fa051132015b
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-02-18 19:20:24 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
+
+async function findOneByUsername(username){
+    const userFound = await runSelectQuery(username);
+    return userFound;
+}
+
+async function runSelectQuery(username) {
+
+    const results = await database.query({
+        text: `SELECT
+                * 
+              FROM 
+                users 
+              WHERE 
+                LOWER(username) = LOWER($1)
+              LIMIT
+                1
+        ;`,
+        values: [username]
+    });
+    if(results.length === 0) {
+        throw new NotFoundError({
+            message: 'O username informado não foi encontrado no sistema.',
+            action: 'Verifique se o username está digitado corretamente.',
+        })
+    }
+    return results[0]
+}
 
 async function create(userInputValues){
     await  validateUniqueEmail(userInputValues.email)
@@ -70,7 +98,8 @@ async function runInsertQuery(userInputValues) {
 }
 
 const user = {
-    create
+    create,
+    findOneByUsername
 };
 
 export default user;
