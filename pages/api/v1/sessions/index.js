36f48efa6914b7de7d4180c20e7b08fc58e1144e
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-02-25 11:22:28 -0300

    feat: create a `Authentication` and `sessions`

---
+++ pages/api/v1/sessions/index.js
@@ -0,0 +1,28 @@
+import controller from 'infra/controller';
+import * as cookie from 'cookie';
+import authentication from 'models/authentication';
+import session from 'models/session';
+import { createRouter } from 'next-connect';
+
+const router = createRouter();
+
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(req, res) {
+  const userInputValues = req.body;
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password
+  );
+  const newSession = await session.create(authenticatedUser.id);
+  const setCookie = cookie.serialize('session_id', newSession.token, {
+    path: '/',
+    maxAge: session.EXPIRATION_IN_MILISECONDS / 1000,
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+  });
+  res.setHeader('Set-Cookie', setCookie);
+  res.status(201).json(newSession);
+}
