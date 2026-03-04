49f7dfa5f3f84dc2cea1d88cc8371e3c68801990
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-03-04 20:35:26 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -70,6 +70,21 @@ describe('Use case: Registration Flow (all successful)', () => {
     });
 
     test('Login', async () => {
+      const createSessionsResponse = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@gmail.com",
+          password: "RegistrationFlowPassword",
+        })
+      })
+
+      expect(createSessionsResponse.status).toBe(201);
+
+      const createSessionsResponseBody = await createSessionsResponse.json();
+      expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
     });
 
     test('Get user information', async () => {
