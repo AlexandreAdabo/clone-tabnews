b8a578f10fbafaa87b7515169242d51261fb4310
AlexandreAdabo <alexandreadabo_@hotmail.com>
2026-03-04 10:55:11 -0300

    feat: add `email` infra and tests

---
+++ tests/integration/infra/email.test.js
@@ -0,0 +1,33 @@
+import email from 'infra/email';
+import orchestrator from '../api/v1/orchestrator';
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe('infra/email.js', () => {
+  test('send()', async () => {
+    await orchestrator.deleteAllEmails();
+    await email.send({
+      from: 'Fintab <contato@fintab.com.br>',
+      to: '<contato@curso.dev>',
+      subject: 'Teste de assunto',
+      text: 'Teste de corpo',
+      //html: '<h1>Teste de HTML</h1>',
+    });
+
+    await email.send({
+      from: 'Fintab <contato@fintab.com.br>',
+      to: '<contato@curso.dev>',
+      subject: 'último email enviado',
+      text: 'Corpo do último email',
+      //html: '<h1>Teste de HTML</h1>',
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+    expect(lastEmail.sender).toBe('<contato@fintab.com.br>');
+    expect(lastEmail.recipients[0]).toBe('<contato@curso.dev>');
+    expect(lastEmail.subject).toBe('último email enviado');
+    expect(lastEmail.text).toBe('Corpo do último email\r\n');
+  });
+});
